495f45d32dbd92079adda5c6324478f71e708e18
MD. NADIMUL ISLAM <nadim015582@gmail.com>
2026-05-06 01:28:33 +0600

    Fix indentation for App component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <AuthProvider>
       <CaptainsProvider>
         <ToastProvider>
-    <App />
+          <App />
         </ToastProvider>
       </CaptainsProvider>
     </AuthProvider>
