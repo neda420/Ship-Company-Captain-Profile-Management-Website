b092f42fb66aa97b79c7a62dddb757f7db00f1d2
MD. NADIMUL ISLAM <nadim015582@gmail.com>
2026-07-19 13:27:18 +0600

    Fix syntax error in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
-  {
+  { 
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
